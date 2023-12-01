fb1eaa6ba423a7cb6e3eff2b3e4cb6f4c8052bd7
Victória Rocha Brito <victoria.rochabrito@gmail.com>
2023-12-01 14:12:32 -0300

    Laboratório Rest e MQTT

---
+++ workspace_eclipse/lab_mqtt/src/main/java/br/inatel/labs/lab_mqtt/client/SensorTemperaturaSubscriber.java
@@ -0,0 +1,39 @@
+package br.inatel.labs.lab_mqtt.client;
+
+import java.util.UUID;
+
+import org.eclipse.paho.client.mqttv3.IMqttClient;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+
+public class SensorTemperaturaSubscriber {
+
+	public static void main(String[] args) {
+		String subscriberId = UUID.randomUUID().toString();
+		
+		IMqttClient subscriber = null;
+		try {
+			subscriber = new MqttClient( MyConstants.URI_BROKER, subscriberId);
+			MqttConnectOptions options = new MqttConnectOptions();
+			options.setAutomaticReconnect(true);
+			options.setCleanSession(true);
+			options.setConnectionTimeout(10);
+			subscriber.connect(options);
+			System.out.println("Subscriber conectado e aguardando msg...");
+
+			subscriber.subscribe(MyConstants.TOPIC_SENSOR, (topic, msg) -> {
+				System.out.println( "Msg recebido: " + msg );
+				System.out.println( "Topico recebido: " + topic );
+			});
+		} catch (MqttException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				subscriber.close();
+			} catch (MqttException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
